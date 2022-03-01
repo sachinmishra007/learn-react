6389bdc6118cf0b5bca1358ad8a61c0726f12708
Sachin Mishra <sachinmishra609@gmail.com>
2022-03-01 18:19:05 +0530

    intial commit

--- src/components/todo/main-todo.js
+++ src/components/todo/main-todo.js
@@ -11,7 +11,7 @@ const MainTodo = () => {
         display: false,
         message: ''
     });
-
+   
     const [todoList, setTodoList] = useState([]);
 
     const onOperation = (event, operation) => {
